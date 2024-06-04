26c7a2cde1425546ab879c9c80ecadffc6ad9eba
dipishBisht <dipish1718@gmail.com>
2024-06-04 17:33:17 +0530

    sign up page hardcode data added

--- src/Utils/Utilities.tsx
+++ src/Utils/Utilities.tsx
@@ -1,3 +1,36 @@
+import airConditioner from "../assets/Air Conditioner.jpg";
+import refrigerator from "../assets/Refrigerators.jpg";
+import microwave from "../assets/Microwaves.jpg";
+import washingMachine from "../assets/Washing Machine.jpg";
+import starting1 from "../assets/Starting-1.jpg";
+import starting2 from "../assets/Starting-2.jpg";
+import starting3 from "../assets/Starting-3.jpg";
+import starting4 from "../assets/Starting-4.jpg";
+import automatic1 from "../assets/Automatic-1.jpg";
+import automatic2 from "../assets/Automatic-2.jpg";
+import automatic3 from "../assets/Automatic-3.jpg";
+import automatic4 from "../assets/Automatic-4.jpg";
+import startingi from "../assets/Starting-i.jpg";
+import startingii from "../assets/Starting-ii.jpg";
+import startingiii from "../assets/Starting-iii.jpg";
+import startingiv from "../assets/Starting-iv.jpg";
+import revanmp1 from "../assets/Revamp-1.jpg";
+import revanmp2 from "../assets/Revamp-2.jpg";
+import revanmp3 from "../assets/Revamp-3.jpg";
+import revanmp4 from "../assets/Revamp-4.jpg";
+import women1 from "../assets/Womens-1.jpg";
+import women2 from "../assets/Womens-2.jpg";
+import women3 from "../assets/Womens-3.jpg";
+import women4 from "../assets/Womens-4.jpg";
+import professional1 from "../assets/Professional-1.jpg";
+import professional2 from "../assets/Professional-2.jpg";
+import professional3 from "../assets/Professional-3.jpg";
+import professional4 from "../assets/Professional-4.jpg";
+import baby1 from "../assets/Baby-1.jpg";
+import baby2 from "../assets/Baby-2.jpg";
+import baby3 from "../assets/Baby-3.jpg";
+import baby4 from "../assets/Baby-4.jpg";
+
 export const options: string[] = [
   "All",
   "Alexa Skills",
@@ -56,4 +89,194 @@ export const langOptions: string[] = [
   "മലയാളം - ML",
   "বাংলা - BN",
   "मराठी - MR",
+];
+
+export const subNavbarList: string[] = [
+  "All",
+  "Fresh",
+  "Amazon miniTV",
+  "Sell",
+  "Best Sellers",
+  "Mobiles",
+  "Todays Deals",
+  "Electronics",
+  "Fasion",
+  "Prime",
+  "Customer Service",
+  "Home & Kitchen",
+  "New Releases",
+];
+
+export const productList1 = [
+  {
+    title: "Appliances for your home | Up to 55% off",
+    subProducts: [
+      {
+        subTitle: "Air Conditioners",
+        subImage: airConditioner,
+      },
+      {
+        subTitle: "Refrigerators",
+        subImage: refrigerator,
+      },
+      {
+        subTitle: "Microwaves",
+        subImage: microwave,
+      },
+      {
+        subTitle: "Washing Machine",
+        subImage: washingMachine,
+      },
+    ],
+  },
+  {
+    title: "Starting ₹99 | Amazon Brands & more",
+    subProducts: [
+      {
+        subTitle: "Starting ₹179 | Home decor",
+        subImage: starting1,
+      },
+      {
+        subTitle: "Up to 60% off | Storage & racks",
+        subImage: starting2,
+      },
+      {
+        subTitle: "Starting ₹99 | Tous & games",
+        subImage: starting3,
+      },
+      {
+        subTitle: "Up to 60% off | Jackets,dresses & ...",
+        subImage: starting4,
+      },
+    ],
+  },
+  {
+    title: "Automotive essentials | Up to 60% off",
+    subProducts: [
+      {
+        subTitle: "Cleaning Assesories",
+        subImage: automatic1,
+      },
+      {
+        subTitle: "Tyre & rim care",
+        subImage: automatic2,
+      },
+      {
+        subTitle: "Helmets",
+        subImage: automatic3,
+      },
+      {
+        subTitle: "Vacuum Cleaner",
+        subImage: automatic4,
+      },
+    ],
+  },
+  {
+    title: "Starting ₹199 | Amazon Brands & more",
+    subProducts: [
+      {
+        subTitle: "Starting ₹199 | Bedsheets",
+        subImage: startingi,
+      },
+      {
+        subTitle: "Starting ₹199 | Curtains",
+        subImage: startingii,
+      },
+      {
+        subTitle: "Min. 40% off | Ironing boards & more",
+        subImage: startingiii,
+      },
+      {
+        subTitle: "Up to 60% off | Home decor",
+        subImage: startingiv,
+      },
+    ],
+  },
+];
+
+export const productList2 = [
+  {
+    title: "Revamp your home in style",
+    subProducts: [
+      {
+        subTitle: "Cushion covers, bedsheets & more",
+        subImage: revanmp1,
+      },
+      {
+        subTitle: "Figurines, vases and more",
+        subImage: revanmp2,
+      },
+      {
+        subTitle: "Home storage",
+        subImage: revanmp3,
+      },
+      {
+        subTitle: "Lighting solutions",
+        subImage: revanmp4,
+      },
+    ],
+  },
+  {
+    title: "Up to 60% off | Styles for women",
+    subProducts: [
+      {
+        subTitle: "Womens Clothing",
+        subImage: women1,
+      },
+      {
+        subTitle: "Footwear + Handbags",
+        subImage: women2,
+      },
+      {
+        subTitle: "Watches",
+        subImage: women3,
+      },
+      {
+        subTitle: "Fashion Jewellery",
+        subImage: women4,
+      },
+    ],
+  },
+  {
+    title: "Up to 60% off | Professional tools, testin...",
+    subProducts: [
+      {
+        subTitle: "Professional Tools",
+        subImage: professional1,
+      },
+      {
+        subTitle: "Measuring Instruments",
+        subImage: professional2,
+      },
+      {
+        subTitle: "Cleaning Supplies",
+        subImage: professional3,
+      },
+      {
+        subTitle: "Medical Supplies",
+        subImage: professional4,
+      },
+    ],
+  },
+  {
+    title: "Min. 40% off | Baby essentials | Amazon...",
+    subProducts: [
+      {
+        subTitle: "Min. 50% off | Diapers & wipes",
+        subImage: baby1,
+      },
+      {
+        subTitle: "Min. 50% off | Cradles, rockers & ...",
+        subImage: baby2,
+      },
+      {
+        subTitle: "Min. 40% off | Toys ",
+        subImage: baby3,
+      },
+      {
+        subTitle: "Visit the store",
+        subImage: baby4,
+      },
+    ],
+  },
 ];
