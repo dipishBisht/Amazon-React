67c53d4068a28f0dc6ae2b16143bdd0008ea252c
dipishBisht <dipish1718@gmail.com>
2024-06-05 18:57:12 +0530

    Sign in page components added

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -1,15 +1,30 @@
 import HomeSlider from "../Components/HomeSlider";
+import CommonSlider from "../Components/CommonSlider";
 import Navbar from "../Components/Navbar";
 import ProductComponent1 from "../Components/ProductComponent1";
 import ProductComponent2 from "../Components/ProductComponent2";
+import HomeData from "../Components/HomeData";
+import {
+  electricImgs,
+  kitchenImgs,
+  trends,
+  clothing,
+} from "../Utils/Utilities";
+import VideoComponent from "../Components/VideoComponent";
 
 const SignIn = () => {
   return (
-    <div>
+    <div className="bg-[#e3e6e6]">
       <Navbar />
       <HomeSlider />
       <ProductComponent1 />
       <ProductComponent2 />
+      <CommonSlider arrayName={electricImgs} />
+      <CommonSlider arrayName={kitchenImgs} />
+      <HomeData />
+      <CommonSlider arrayName={trends} />
+      <CommonSlider arrayName={clothing} />
+      <VideoComponent />
     </div>
   );
 };
