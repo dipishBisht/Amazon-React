ab2078920a129a8eb72feef13cb4926379511275
dipishBisht <dipish1718@gmail.com>
2024-06-05 18:57:51 +0530

    hardcodded data of components added

--- src/Utils/Utilities.tsx
+++ src/Utils/Utilities.tsx
@@ -30,6 +30,106 @@ import baby1 from "../assets/Baby-1.jpg";
 import baby2 from "../assets/Baby-2.jpg";
 import baby3 from "../assets/Baby-3.jpg";
 import baby4 from "../assets/Baby-4.jpg";
+import Electric1 from "../assets/Electric-1.jpg";
+import Electric2 from "../assets/Electric-2.jpg";
+import Electric3 from "../assets/Electric-3.jpg";
+import Electric4 from "../assets/Electric-4.jpg";
+import Electric5 from "../assets/Electric-5.jpg";
+import Electric6 from "../assets/Electric-6.jpg";
+import Electric7 from "../assets/Electric-7.jpg";
+import Electric8 from "../assets/Electric-8.jpg";
+import Electric9 from "../assets/Electric-9.jpg";
+import Electric10 from "../assets/Electric-10.jpg";
+import Electric11 from "../assets/Electric-11.jpg";
+import Electric12 from "../assets/Electric-12.jpg";
+import Electric13 from "../assets/Electric-13.jpg";
+import Electric14 from "../assets/Electric-14.jpg";
+import Electric15 from "../assets/Electric-15.jpg";
+import Electric16 from "../assets/Electric-16.jpg";
+import Electric17 from "../assets/Electric-17.jpg";
+import Electric18 from "../assets/Electric-18.jpg";
+import Electric19 from "../assets/Electric-19.jpg";
+import Electric20 from "../assets/Electric-20.jpg";
+import Electric21 from "../assets/Electric-21.jpg";
+import Electric22 from "../assets/Electric-22.jpg";
+import Electric23 from "../assets/Electric-23.jpg";
+import Electric24 from "../assets/Electric-24.jpg";
+import Kitchen1 from "../assets/Kitchen-1.jpg";
+import Kitchen2 from "../assets/Kitchen-2.jpg";
+import Kitchen3 from "../assets/Kitchen-3.jpg";
+import Kitchen4 from "../assets/Kitchen-4.jpg";
+import Kitchen5 from "../assets/Kitchen-5.jpg";
+import Kitchen6 from "../assets/Kitchen-6.jpg";
+import Kitchen7 from "../assets/Kitchen-7.jpg";
+import Kitchen8 from "../assets/Kitchen-8.jpg";
+import Kitchen9 from "../assets/Kitchen-9.jpg";
+import Kitchen10 from "../assets/Kitchen-10.jpg";
+import Kitchen11 from "../assets/Kitchen-11.jpg";
+import Kitchen12 from "../assets/Kitchen-12.jpg";
+import Kitchen13 from "../assets/Kitchen-13.jpg";
+import Kitchen14 from "../assets/Kitchen-14.jpg";
+import Kitchen15 from "../assets/Kitchen-15.jpg";
+import Kitchen16 from "../assets/Kitchen-16.jpg";
+import Kitchen17 from "../assets/Kitchen-17.jpg";
+import Kitchen18 from "../assets/Kitchen-18.jpg";
+import Kitchen19 from "../assets/Kitchen-19.jpg";
+import Kitchen20 from "../assets/Kitchen-20.jpg";
+import Kitchen21 from "../assets/Kitchen-21.jpg";
+import Kitchen22 from "../assets/Kitchen-22.jpg";
+import Kitchen23 from "../assets/Kitchen-23.jpg";
+import Kitchen24 from "../assets/Kitchen-24.jpg";
+import Kitchen25 from "../assets/Kitchen-25.jpg";
+import Kitchen26 from "../assets/Kitchen-26.jpg";
+import Kitchen27 from "../assets/Kitchen-27.jpg";
+import Kitchen28 from "../assets/Kitchen-28.jpg";
+import Kitchen29 from "../assets/Kitchen-29.jpg";
+import Kitchen30 from "../assets/Kitchen-30.jpg";
+import Kitchen31 from "../assets/Kitchen-31.jpg";
+import Kitchen32 from "../assets/Kitchen-32.jpg";
+import HomeData1 from "../assets/HomeData-1.jpg";
+import HomeData2 from "../assets/HomeData-2.jpg";
+import HomeData3 from "../assets/HomeData-3.jpg";
+import HomeData4 from "../assets/HomeData-4.jpg";
+import HomeData5 from "../assets/HomeData-5.jpg";
+import HomeData6 from "../assets/HomeData-6.jpg";
+import HomeData7 from "../assets/HomeData-7.jpg";
+import HomeData8 from "../assets/HomeData-8.jpg";
+import HomeData9 from "../assets/HomeData-9.jpg";
+import HomeData10 from "../assets/HomeData-10.jpg";
+import Trends1 from "../assets/Trends-1.jpg";
+import Trends2 from "../assets/Trends-2.jpg";
+import Trends3 from "../assets/Trends-3.jpg";
+import Trends4 from "../assets/Trends-4.jpg";
+import Trends5 from "../assets/Trends-5.jpg";
+import Trends6 from "../assets/Trends-6.jpg";
+import Trends7 from "../assets/Trends-7.jpg";
+import Trends8 from "../assets/Trends-8.jpg";
+import Trends9 from "../assets/Trends-9.jpg";
+import Trends10 from "../assets/Trends-10.jpg";
+import Trends11 from "../assets/Trends-11.jpg";
+import Trends12 from "../assets/Trends-12.jpg";
+import Trends13 from "../assets/Trends-13.jpg";
+import Clothing1 from "../assets/Clothing-1.jpg";
+import Clothing2 from "../assets/Clothing-2.jpg";
+import Clothing3 from "../assets/Clothing-3.jpg";
+import Clothing4 from "../assets/Clothing-4.jpg";
+import Clothing5 from "../assets/Clothing-5.jpg";
+import Clothing6 from "../assets/Clothing-6.jpg";
+import Clothing7 from "../assets/Clothing-7.jpg";
+import Clothing8 from "../assets/Clothing-8.jpg";
+import Clothing9 from "../assets/Clothing-9.jpg";
+import Clothing10 from "../assets/Clothing-10.jpg";
+import Clothing11 from "../assets/Clothing-11.jpg";
+import Clothing12 from "../assets/Clothing-12.jpg";
+import Clothing13 from "../assets/Clothing-13.jpg";
+import Clothing14 from "../assets/Clothing-14.jpg";
+import Clothing15 from "../assets/Clothing-15.jpg";
+import Clothing16 from "../assets/Clothing-16.jpg";
+import Clothing17 from "../assets/Clothing-17.jpg";
+import Clothing18 from "../assets/Clothing-18.jpg";
+import Clothing19 from "../assets/Clothing-19.jpg";
+import Clothing20 from "../assets/Clothing-20.jpg";
+import VideoImage from "../assets/home-video.jpeg";
 
 export const options: string[] = [
   "All",
@@ -280,3 +380,146 @@ export const productList2 = [
     ],
   },
 ];
+
+export const electricImgs = [
+  {
+    title: "Electric scooters & motorbikes",
+    imgs: [
+      Electric1,
+      Electric2,
+      Electric3,
+      Electric4,
+      Electric5,
+      Electric6,
+      Electric7,
+      Electric8,
+      Electric9,
+      Electric10,
+      Electric11,
+      Electric12,
+      Electric13,
+      Electric14,
+      Electric15,
+      Electric16,
+      Electric17,
+      Electric18,
+      Electric19,
+      Electric20,
+      Electric21,
+      Electric22,
+      Electric23,
+      Electric24,
+    ],
+  },
+];
+
+export const kitchenImgs = [
+  {
+    title: "Minimum 50% off | Kitchen needs curated from stores nearby",
+    imgs: [
+      Kitchen1,
+      Kitchen2,
+      Kitchen3,
+      Kitchen4,
+      Kitchen5,
+      Kitchen6,
+      Kitchen7,
+      Kitchen8,
+      Kitchen9,
+      Kitchen10,
+      Kitchen11,
+      Kitchen12,
+      Kitchen13,
+      Kitchen14,
+      Kitchen15,
+      Kitchen16,
+      Kitchen17,
+      Kitchen18,
+      Kitchen19,
+      Kitchen20,
+      Kitchen21,
+      Kitchen22,
+      Kitchen23,
+      Kitchen24,
+      Kitchen25,
+      Kitchen26,
+      Kitchen27,
+      Kitchen28,
+      Kitchen29,
+      Kitchen30,
+      Kitchen31,
+      Kitchen32,
+    ],
+  },
+];
+
+export const homeData = [
+  {
+    title: "Under ₹699 | Combo packs | Amazon brands ...",
+    images: [HomeData1],
+  },
+  {
+    title: "Customers’ Most-Loved Fashion for you",
+    images: [HomeData2, HomeData3, HomeData4, HomeData5],
+  },
+  {
+    title: "Up to 50% off | International brands",
+    images: [HomeData10],
+  },
+  {
+    title: "Under ₹499 | Pocket-friendly fashion",
+    images: [HomeData6, HomeData7, HomeData8, HomeData9],
+  },
+];
+
+export const trends = [
+  {
+    title:
+      "Up to 80% off | Fashionable and trendy clothes from Small businesses",
+    imgs: [
+      Trends1,
+      Trends2,
+      Trends3,
+      Trends4,
+      Trends5,
+      Trends6,
+      Trends7,
+      Trends8,
+      Trends9,
+      Trends10,
+      Trends11,
+      Trends12,
+      Trends13,
+    ],
+  },
+];
+
+export const clothing = [
+  {
+    title: "Best Sellers in Clothing & Accessories",
+    imgs: [
+      Clothing1,
+      Clothing2,
+      Clothing3,
+      Clothing4,
+      Clothing5,
+      Clothing6,
+      Clothing7,
+      Clothing8,
+      Clothing9,
+      Clothing10,
+      Clothing11,
+      Clothing12,
+      Clothing13,
+      Clothing14,
+      Clothing15,
+      Clothing16,
+      Clothing17,
+      Clothing18,
+      Clothing19,
+      Clothing20,
+    ],
+  },
+];
+
+export const videoImg = VideoImage;
